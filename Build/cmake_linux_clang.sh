2edda7383304c54e2139718aba05b86b28f9d024
Jorrit Rouwe <jrouwe@gmail.com>
2021-08-13 15:44:45 +0200

    Test different compiler versions

--- Build/cmake_linux_clang.sh
+++ Build/cmake_linux_clang.sh
@@ -14,6 +14,6 @@ echo Usage: ./cmake_linux_clang.sh [Configuration]
 echo "Possible configurations: Debug (default), Release, Distribution, ReleaseUBSAN, ReleaseASAN, ReleaseCoverage"
 echo Generating Makefile for build type \"$BUILD_TYPE\" in folder \"$BUILD_DIR\"
 
-cmake -S . -B $BUILD_DIR -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+cmake -S . -B $BUILD_DIR -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=clang++
 
 echo Compile by running \"make -j 8 \&\& ./UnitTests\" in folder \"$BUILD_DIR\"
