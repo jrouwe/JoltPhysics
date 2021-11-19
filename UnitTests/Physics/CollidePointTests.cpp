51c3db5cdcba0315ca477bb153dc25483c948384
Jorrit Rouwe <jrouwe@gmail.com>
2021-11-19 15:40:49 +0100

    Implemented collide point tests for a number of other shapes

--- UnitTests/Physics/CollidePointTests.cpp
+++ UnitTests/Physics/CollidePointTests.cpp
@@ -4,13 +4,186 @@
 #include "UnitTestFramework.h"
 #include "PhysicsTestContext.h"
 #include "Layers.h"
+#include <Physics/Collision/Shape/BoxShape.h>
+#include <Physics/Collision/Shape/SphereShape.h>
+#include <Physics/Collision/Shape/CapsuleShape.h>
+#include <Physics/Collision/Shape/CylinderShape.h>
+#include <Physics/Collision/Shape/TaperedCapsuleShape.h>
 #include <Physics/Collision/Shape/MeshShape.h>
 #include <Physics/Collision/CollisionCollectorImpl.h>
 #include <Physics/Collision/CollidePointResult.h>
 
 TEST_SUITE("CollidePointTests")
 {
-	// Test the test framework's helper functions
+	// Probe directions in the direction of the faces
+	static Vec3 cube_probes[] = {
+		Vec3(-1.0f, 0, 0),
+		Vec3(1.0f, 0, 0),
+		Vec3(0, -1.0f, 0),
+		Vec3(0, 1.0f, 0),
+		Vec3(0, 0, -1.0f),
+		Vec3(0, 0, 1.0f)
+	};
+
+	// Probe directions in the direction of the faces
+	static Vec3 cube_and_zero_probes[] = {
+		Vec3(0, 0, 0),
+		Vec3(-1.0f, 0, 0),
+		Vec3(1.0f, 0, 0),
+		Vec3(0, -1.0f, 0),
+		Vec3(0, 1.0f, 0),
+		Vec3(0, 0, -1.0f),
+		Vec3(0, 0, 1.0f)
+	};
+
+	// Probes in the xy-plane
+	static Vec3 xy_probes[] = {
+		Vec3(-1.0f, 0, 0),
+		Vec3(1.0f, 0, 0),
+		Vec3(0, 0, -1.0f),
+		Vec3(0, 0, 1.0f)
+	};
+
+	// Probes in the xy-plane and zero
+	static Vec3 xy_and_zero_probes[] = {
+		Vec3(0, 0, 0),
+		Vec3(-1.0f, 0, 0),
+		Vec3(1.0f, 0, 0),
+		Vec3(0, 0, -1.0f),
+		Vec3(0, 0, 1.0f)
+	};
+
+	static void sTestHit(const Shape *inShape, Vec3Arg inPosition)
+	{
+		AllHitCollisionCollector<CollidePointCollector> collector;
+		inShape->CollidePoint(inPosition - inShape->GetCenterOfMass(), SubShapeIDCreator(), collector);
+		CHECK(collector.mHits.size() == 1);
+	}
+
+	static void sTestHit(const NarrowPhaseQuery &inNarrowPhase, Vec3Arg inPosition, const BodyID &inBodyID)
+	{
+		AllHitCollisionCollector<CollidePointCollector> collector;
+		inNarrowPhase.CollidePoint(inPosition, collector);
+		CHECK(collector.mHits.size() == 1);
+		CHECK(collector.mHits[0].mBodyID == inBodyID);
+	}
+
+	static void sTestMiss(const Shape *inShape, Vec3Arg inPosition)
+	{
+		AllHitCollisionCollector<CollidePointCollector> collector;
+		inShape->CollidePoint(inPosition - inShape->GetCenterOfMass(), SubShapeIDCreator(), collector);
+		CHECK(collector.mHits.empty());
+	}
+
+	static void sTestMiss(const NarrowPhaseQuery &inNarrowPhase, Vec3Arg inPosition)
+	{
+		AllHitCollisionCollector<CollidePointCollector> collector;
+		inNarrowPhase.CollidePoint(inPosition, collector);
+		CHECK(collector.mHits.empty());
+	}
+
+	TEST_CASE("TestCollidePointVsBox")
+	{
+		Vec3 half_box_size(0.1f, 0.2f, 0.3f);
+		ShapeRefC shape = new BoxShape(half_box_size);
+
+		// Hits
+		for (Vec3 probe : cube_and_zero_probes)
+			sTestHit(shape, 0.99f * half_box_size * probe);
+
+		// Misses
+		for (Vec3 probe : cube_probes)
+			sTestMiss(shape, 1.01f * half_box_size * probe);
+	}
+
+	TEST_CASE("TestCollidePointVsSphere")
+	{
+		const float radius = 0.1f;
+		ShapeRefC shape = new SphereShape(radius);
+
+		// Hits
+		for (Vec3 probe : cube_and_zero_probes)
+			sTestHit(shape, 0.99f * Vec3::sReplicate(radius) * probe);
+
+		// Misses
+		for (Vec3 probe : cube_probes)
+			sTestMiss(shape, 1.01f * Vec3::sReplicate(radius) * probe);
+	}
+
+	TEST_CASE("TestCollidePointVsCapsule")
+	{
+		const float half_height = 0.2f;
+		const float radius = 0.1f;
+		ShapeRefC shape = new CapsuleShape(half_height, radius);
+
+		// Top hits
+		for (Vec3 probe : xy_and_zero_probes)
+			sTestHit(shape, 0.99f * radius * probe + Vec3(0, half_height, 0));
+
+		// Center hit
+		sTestHit(shape, Vec3::sZero());
+
+		// Bottom hits
+		for (Vec3 probe : xy_and_zero_probes)
+			sTestHit(shape, 0.99f * radius * probe + Vec3(0, -half_height, 0));
+
+		// Misses
+		for (Vec3 probe : cube_probes)
+			sTestMiss(shape, 1.01f * Vec3(radius, half_height + radius, radius) * probe);
+	}
+
+	TEST_CASE("TestCollidePointVsTaperedCapsule")
+	{
+		const float half_height = 0.4f;
+		const float top_radius = 0.1f;
+		const float bottom_radius = 0.2f;
+		TaperedCapsuleShapeSettings settings(half_height, top_radius, bottom_radius);
+		ShapeRefC shape = settings.Create().Get();
+
+		// Top hits
+		for (Vec3 probe : xy_and_zero_probes)
+			sTestHit(shape, 0.99f * top_radius * probe + Vec3(0, half_height, 0));
+
+		// Center hit
+		sTestHit(shape, Vec3::sZero());
+
+		// Bottom hits
+		for (Vec3 probe : xy_and_zero_probes)
+			sTestHit(shape, 0.99f * bottom_radius * probe + Vec3(0, -half_height, 0));
+
+		// Top misses
+		sTestMiss(shape, Vec3(0, half_height + top_radius + 0.01f, 0));
+		for (Vec3 probe : xy_probes)
+			sTestMiss(shape, 1.01f * top_radius * probe + Vec3(0, half_height, 0));
+
+		// Bottom misses
+		sTestMiss(shape, Vec3(0, -half_height - bottom_radius - 0.01f, 0));
+		for (Vec3 probe : xy_probes)
+			sTestMiss(shape, 1.01f * bottom_radius * probe + Vec3(0, -half_height, 0));
+	}
+
+	TEST_CASE("TestCollidePointVsCylinder")
+	{
+		const float half_height = 0.2f;
+		const float radius = 0.1f;
+		ShapeRefC shape = new CylinderShape(half_height, radius);
+
+		// Top hits
+		for (Vec3 probe : xy_and_zero_probes)
+			sTestHit(shape, 0.99f * (radius * probe + Vec3(0, half_height, 0)));
+
+		// Center hit
+		sTestHit(shape, Vec3::sZero());
+
+		// Bottom hits
+		for (Vec3 probe : xy_and_zero_probes)
+			sTestHit(shape, 0.99f * (radius * probe + Vec3(0, -half_height, 0)));
+
+		// Misses
+		for (Vec3 probe : cube_probes)
+			sTestMiss(shape, 1.01f * Vec3(radius, half_height, radius) * probe);
+	}
+
 	TEST_CASE("TestCollidePointVsMesh")
 	{
 		// Vertices of a cube
@@ -39,16 +212,6 @@ TEST_SUITE("CollidePointTests")
 			{ 1, 5, 7 },
 			{ 0, 2, 6 },
 			{ 0, 6, 4 }
-		};
-
-		// Probe directions in the direction of the faces
-		Vec3 probes[] = {
-			Vec3(-1.0f, 0, 0),
-			Vec3(1.0f, 0, 0),
-			Vec3(0, -1.0f, 0),
-			Vec3(0, 1.0f, 0),
-			Vec3(0, 0, -1.0f),
-			Vec3(0, 0, 1.0f)
 		};
 
 		const int grid_size = 2;
@@ -109,38 +272,15 @@ TEST_SUITE("CollidePointTests")
 					Vec3 center((float)x, (float)y, (float)z);
 
 					// The center point should hit
-					{
-						// Test shape directly
-						AllHitCollisionCollector<CollidePointCollector> collector;
-						mesh_shape->CollidePoint(center, SubShapeIDCreator(), collector);
-						CHECK(collector.mHits.size() == 1);
-
-						// Reset collector
-						collector.Reset();
-						CHECK(collector.mHits.empty());
-
-						// Also not when going through the narrow phase
-						narrow_phase.CollidePoint(body_transform * center, collector);
-						CHECK(collector.mHits.size() == 1);
-						CHECK(collector.mHits[0].mBodyID == mesh_body.GetID());
-					}
+					sTestHit(mesh_shape, center);
+					sTestHit(narrow_phase, body_transform * center, mesh_body.GetID());
 
 					// Points outside the hull should not hit
-					for (Vec3 probe : probes)
+					for (Vec3 probe : cube_probes)
 					{
-						// Test shape directly
-						AllHitCollisionCollector<CollidePointCollector> collector;
 						Vec3 point = center + 0.4f * probe;
-						mesh_shape->CollidePoint(point, SubShapeIDCreator(), collector);
-						CHECK(collector.mHits.empty());
-
-						// Reset collector
-						collector.Reset();
-						CHECK(collector.mHits.empty());
-
-						// Also not when going through the narrow phase
-						narrow_phase.CollidePoint(body_transform * point, collector);
-						CHECK(collector.mHits.empty());
+						sTestMiss(mesh_shape, point);
+						sTestMiss(narrow_phase, body_transform * point);
 					}
 				}
 	}
