fe15c42c80aad4fb5b016634a7e6a5eedd739c8c
mrezai <mhd.rezai@gmail.com>
2022-02-24 23:07:11 +0330

    Fix compile error when lzcnt isn't enabled (#59)

--- Jolt/Core/Core.h
+++ Jolt/Core/Core.h
@@ -14,8 +14,10 @@
 	#define JPH_PLATFORM_LINUX
 #endif
 
-// Turn off warnings
+// Determine compiler and turn off warnings
 #if defined(__clang__)
+	#define JPH_COMPILER_CLANG
+
 	#pragma clang diagnostic ignored "-Wc++98-compat"
 	#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
 	#pragma clang diagnostic ignored "-Wfloat-equal"
@@ -40,6 +42,8 @@
 		#pragma clang diagnostic ignored "-Wimplicit-int-float-conversion"
 	#endif
 #elif defined(_MSC_VER)
+	#define JPH_COMPILER_MSVC
+
 	#pragma warning (disable : 4514) // 'X' : unreferenced inline function has been removed
 	#pragma warning (disable : 4710) // 'X' : function not inlined
 	#pragma warning (disable : 4711) // function 'X' selected for automatic inline expansion
