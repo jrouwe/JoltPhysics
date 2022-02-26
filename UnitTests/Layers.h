ee3f2fc9e710053524c78c9ad390209d33e8ac93
Jorrit Rouwe <jrouwe@gmail.com>
2022-02-26 20:37:55 +0100

    Added unit test for layer switching

--- UnitTests/Layers.h
+++ UnitTests/Layers.h
@@ -9,12 +9,16 @@
 /// Layer that objects can be in, determines which other objects it can collide with
 namespace Layers
 {
-	static constexpr uint8 UNUSED1 = 0; // 3 unused values so that broadphase layers values don't match with object layer values (for testing purposes)
+	static constexpr uint8 UNUSED1 = 0; // 5 unused values so that broadphase layers values don't match with object layer values (for testing purposes)
 	static constexpr uint8 UNUSED2 = 1;
 	static constexpr uint8 UNUSED3 = 2;
-	static constexpr uint8 NON_MOVING = 3;
-	static constexpr uint8 MOVING = 4;
-	static constexpr uint8 NUM_LAYERS = 5;
+	static constexpr uint8 UNUSED4 = 3;
+	static constexpr uint8 UNUSED5 = 4;
+	static constexpr uint8 NON_MOVING = 5;
+	static constexpr uint8 MOVING = 6;
+	static constexpr uint8 HQ_DEBRIS = 7; // High quality debris collides with MOVING and NON_MOVING but not with any debris
+	static constexpr uint8 LQ_DEBRIS = 8; // Low quality debris only collides with NON_MOVING
+	static constexpr uint8 NUM_LAYERS = 9;
 };
 
 /// Function that determines if two object layers can collide
@@ -25,11 +29,17 @@ inline bool ObjectCanCollide(ObjectLayer inObject1, ObjectLayer inObject2)
 	case Layers::UNUSED1:
 	case Layers::UNUSED2:
 	case Layers::UNUSED3:
+	case Layers::UNUSED4:
+	case Layers::UNUSED5:
 		return false;
 	case Layers::NON_MOVING:
-		return inObject2 == Layers::MOVING;
+		return inObject2 == Layers::MOVING || inObject2 == Layers::HQ_DEBRIS || inObject2 == Layers::LQ_DEBRIS;
 	case Layers::MOVING:
+		return inObject2 == Layers::NON_MOVING || inObject2 == Layers::MOVING || inObject2 == Layers::HQ_DEBRIS;
+	case Layers::HQ_DEBRIS:
 		return inObject2 == Layers::NON_MOVING || inObject2 == Layers::MOVING;
+	case Layers::LQ_DEBRIS:
+		return inObject2 == Layers::NON_MOVING;
 	default:
 		JPH_ASSERT(false);
 		return false;
@@ -40,9 +50,10 @@ inline bool ObjectCanCollide(ObjectLayer inObject1, ObjectLayer inObject2)
 namespace BroadPhaseLayers
 {
 	static constexpr BroadPhaseLayer NON_MOVING(0);
-	static constexpr BroadPhaseLayer MOVING(1);
-	static constexpr BroadPhaseLayer UNUSED(2);
-	static constexpr uint NUM_LAYERS(3);
+	static constexpr BroadPhaseLayer MOVING(1); 
+	static constexpr BroadPhaseLayer LQ_DEBRIS(2);
+	static constexpr BroadPhaseLayer UNUSED(3);
+	static constexpr uint NUM_LAYERS(4);
 };
 
 /// BroadPhaseLayerInterface implementation
@@ -55,8 +66,12 @@ public:
 		mObjectToBroadPhase[Layers::UNUSED1] = BroadPhaseLayers::UNUSED;
 		mObjectToBroadPhase[Layers::UNUSED2] = BroadPhaseLayers::UNUSED;
 		mObjectToBroadPhase[Layers::UNUSED3] = BroadPhaseLayers::UNUSED;
+		mObjectToBroadPhase[Layers::UNUSED4] = BroadPhaseLayers::UNUSED;
+		mObjectToBroadPhase[Layers::UNUSED5] = BroadPhaseLayers::UNUSED;
 		mObjectToBroadPhase[Layers::NON_MOVING] = BroadPhaseLayers::NON_MOVING;
 		mObjectToBroadPhase[Layers::MOVING] = BroadPhaseLayers::MOVING;
+		mObjectToBroadPhase[Layers::HQ_DEBRIS] = BroadPhaseLayers::MOVING; // HQ_DEBRIS is also in the MOVING layer as an example on how to map multiple layers onto the same broadphase layer
+		mObjectToBroadPhase[Layers::LQ_DEBRIS] = BroadPhaseLayers::LQ_DEBRIS;
 	}
 
 	virtual uint					GetNumBroadPhaseLayers() const override
@@ -77,6 +92,7 @@ public:
 		{
 		case (BroadPhaseLayer::Type)BroadPhaseLayers::NON_MOVING:	return "NON_MOVING";
 		case (BroadPhaseLayer::Type)BroadPhaseLayers::MOVING:		return "MOVING";
+		case (BroadPhaseLayer::Type)BroadPhaseLayers::LQ_DEBRIS:	return "LQ_DEBRIS";
 		case (BroadPhaseLayer::Type)BroadPhaseLayers::UNUSED:		return "UNUSED";
 		default:													JPH_ASSERT(false); return "INVALID";
 		}
@@ -95,10 +111,15 @@ inline bool BroadPhaseCanCollide(ObjectLayer inLayer1, BroadPhaseLayer inLayer2)
 	case Layers::NON_MOVING:
 		return inLayer2 == BroadPhaseLayers::MOVING;
 	case Layers::MOVING:
+	case Layers::HQ_DEBRIS:
 		return inLayer2 == BroadPhaseLayers::NON_MOVING || inLayer2 == BroadPhaseLayers::MOVING;
+	case Layers::LQ_DEBRIS:
+		return inLayer2 == BroadPhaseLayers::NON_MOVING;
 	case Layers::UNUSED1:
 	case Layers::UNUSED2:
 	case Layers::UNUSED3:
+	case Layers::UNUSED4:
+	case Layers::UNUSED5:
 		return false;			
 	default:
 		JPH_ASSERT(false);
