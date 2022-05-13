ce55255fbef5972ac5fe672f55751213423837ce
Joshua Ashton <joshua@froggi.es>
2022-05-13 20:33:25 +0100

    Handle SubShape size difference for 32-bit

--- Jolt/Physics/Collision/Shape/CompoundShape.h
+++ Jolt/Physics/Collision/Shape/CompoundShape.h
@@ -226,7 +226,7 @@ public:
 		// 3 padding bytes left
 	};
 
-	static_assert(sizeof(SubShape) == 40, "Compiler added unexpected padding");
+	static_assert(sizeof(SubShape) == (sizeof(void*) == 8 ? 40 : 36), "Compiler added unexpected padding");
 
 	using SubShapes = vector<SubShape>;
 
