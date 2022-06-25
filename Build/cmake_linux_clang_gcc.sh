564526ad3d426ab8428534d7c9666082e13577d0
Jorrit Rouwe <jrouwe@gmail.com>
2022-06-26 00:00:43 +0200

    Added build to validate determinism between Windows, Linux and MacOS (#189)
    PerformanceTest now supports -validate_hash=... to check the hash.

--- Build/cmake_linux_clang_gcc.sh
+++ Build/cmake_linux_clang_gcc.sh
@@ -5,13 +5,15 @@ then
 	BUILD_TYPE=Debug
 else
 	BUILD_TYPE=$1
+	shift
 fi
 
-if [ -z $2 ] 
+if [ -z $1 ] 
 then
 	COMPILER=clang++
 else
-	COMPILER=$2
+	COMPILER=$1
+	shift
 fi
 
 BUILD_DIR=Linux_$BUILD_TYPE
@@ -21,6 +23,6 @@ echo "Possible configurations: Debug (default), Release, Distribution, ReleaseUB
 echo "Possible compilers: clang++, clang++-XX, g++, g++-XX where XX is the version"
 echo Generating Makefile for build type \"$BUILD_TYPE\" and compiler \"$COMPILER\" in folder \"$BUILD_DIR\"
 
-cmake -S . -B $BUILD_DIR -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=$COMPILER
+cmake -S . -B $BUILD_DIR -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=$COMPILER "${@}"
 
 echo Compile by running \"make -j 8 \&\& ./UnitTests\" in folder \"$BUILD_DIR\"
