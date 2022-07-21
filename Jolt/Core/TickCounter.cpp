b0f967d3b2d672a4a6c29f8778fbf797b2c7782f
Jorrit Rouwe <jrouwe@gmail.com>
2022-07-21 21:54:28 +0200

    Removed whitespace

--- Jolt/Core/TickCounter.cpp
+++ Jolt/Core/TickCounter.cpp
@@ -5,7 +5,7 @@
 
 #include <Jolt/Core/TickCounter.h>
 
-#if defined(JPH_PLATFORM_WINDOWS) 
+#if defined(JPH_PLATFORM_WINDOWS)
 	JPH_SUPPRESS_WARNING_PUSH
 	JPH_MSVC_SUPPRESS_WARNING(5039) // winbase.h(13179): warning C5039: 'TpSetCallbackCleanupGroup': pointer or reference to potentially throwing function passed to 'extern "C"' function under -EHc. Undefined behavior may occur if this function throws an exception.
 	#define WIN32_LEAN_AND_MEAN
