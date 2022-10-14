2a583b6751cb95763635769990b32f2f3bcb9399
Jorrit Rouwe <jrouwe@gmail.com>
2022-10-14 13:36:23 +0200

    Fix warnings for clang 15 (#263)

--- Jolt/Core/Core.h
+++ Jolt/Core/Core.h
@@ -115,14 +115,14 @@
 #define JPH_SUPPRESS_WARNING_PUSH		JPH_PRAGMA(clang diagnostic push)
 #define JPH_SUPPRESS_WARNING_POP		JPH_PRAGMA(clang diagnostic pop)
 #define JPH_CLANG_SUPPRESS_WARNING(w)	JPH_PRAGMA(clang diagnostic ignored w)
-	#if __clang_major__ >= 16
-		#define JPH_CLANG16_SUPPRESS_WARNING(w)	JPH_PRAGMA(clang diagnostic ignored w)
+	#if __clang_major__ >= 15
+		#define JPH_CLANG15_SUPPRESS_WARNING(w)	JPH_PRAGMA(clang diagnostic ignored w)
 	#else
-		#define JPH_CLANG16_SUPPRESS_WARNING(w)
+		#define JPH_CLANG15_SUPPRESS_WARNING(w)
 	#endif
 #else
 #define JPH_CLANG_SUPPRESS_WARNING(w)
-#define JPH_CLANG16_SUPPRESS_WARNING(w)
+#define JPH_CLANG15_SUPPRESS_WARNING(w)
 #endif
 #ifdef JPH_COMPILER_GCC
 #define JPH_PRAGMA(x)					_Pragma(#x)
@@ -162,7 +162,8 @@
 	JPH_CLANG_SUPPRESS_WARNING("-Wgnu-zero-variadic-macro-arguments")							\
 	JPH_CLANG_SUPPRESS_WARNING("-Wdocumentation-unknown-command")								\
 	JPH_CLANG_SUPPRESS_WARNING("-Wctad-maybe-unsupported")										\
-	JPH_CLANG16_SUPPRESS_WARNING("-Wunqualified-std-cast-call")									\
+	JPH_CLANG_SUPPRESS_WARNING("-Wdeprecated-copy")												\
+	JPH_CLANG15_SUPPRESS_WARNING("-Wunqualified-std-cast-call")									\
 	JPH_IF_NOT_ANDROID(JPH_CLANG_SUPPRESS_WARNING("-Wimplicit-int-float-conversion"))			\
 																								\
 	JPH_GCC_SUPPRESS_WARNING("-Wcomment")														\
