71a736e58e1f182530690b175df36089ae9553d9
Jorrit Rouwe <jrouwe@gmail.com>
2022-11-04 16:57:02 +0100

    Implemented floating point control

--- Jolt/Core/FPControlWord.h
+++ Jolt/Core/FPControlWord.h
@@ -30,6 +30,29 @@ private:
 	uint		mPrevState;	
 };
 
+#elif defined(JPH_USE_NEON) && defined(JPH_COMPILER_MSVC)
+
+/// Helper class that needs to be put on the stack to update the state of the floating point control word.
+/// This state is kept per thread.
+template <uint64 Value, uint64 Mask>
+class FPControlWord : public NonCopyable
+{
+public:
+				FPControlWord()
+	{
+		_controlfp_s(&mPrevState, Value, Mask);
+	}
+
+				~FPControlWord()
+	{
+		unsigned int dummy;
+		_controlfp_s(&dummy, mPrevState, Mask);
+	}
+
+private:
+	unsigned int mPrevState;
+};
+
 #elif defined(JPH_USE_NEON)
 
 /// Helper class that needs to be put on the stack to update the state of the floating point control word.
@@ -40,25 +63,21 @@ class FPControlWord : public NonCopyable
 public:
 				FPControlWord()
 	{
-#ifndef JPH_COMPILER_MSVC // TODO
 		uint64 val;
 	    asm volatile("mrs %0, fpcr" : "=r" (val));
 		mPrevState = val;
 		val &= ~Mask;
 		val |= Value;
 	    asm volatile("msr fpcr, %0" : /* no output */ : "r" (val));
-#endif
 	}
 
 				~FPControlWord()
 	{
-#ifndef JPH_COMPILER_MSVC // TODO
 		uint64 val;
 		asm volatile("mrs %0, fpcr" : "=r" (val));
 		val &= ~Mask;
 		val |= mPrevState & Mask;
 		asm volatile("msr fpcr, %0" : /* no output */ : "r" (val));
-#endif
 	}
 
 private:
