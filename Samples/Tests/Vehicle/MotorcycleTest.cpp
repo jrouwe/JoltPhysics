5183d16517d3af150bbb1a1391361f595c82478c
Jorrit Rouwe <jrouwe@gmail.com>
2023-03-12 15:54:54 +0100

    Simple motorcycle test

---
+++ Samples/Tests/Vehicle/MotorcycleTest.cpp
@@ -0,0 +1,168 @@
+// Jolt Physics Library (https://github.com/jrouwe/JoltPhysics)
+// SPDX-FileCopyrightText: 2023 Jorrit Rouwe
+// SPDX-License-Identifier: MIT
+
+#include <TestFramework.h>
+
+#include <Tests/Vehicle/MotorcycleTest.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/OffsetCenterOfMassShape.h>
+#include <Jolt/Physics/Vehicle/WheeledVehicleController.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Layers.h>
+#include <Renderer/DebugRendererImp.h>
+
+JPH_IMPLEMENT_RTTI_VIRTUAL(MotorcycleTest) 
+{ 
+	JPH_ADD_BASE_CLASS(MotorcycleTest, VehicleTest) 
+}
+
+MotorcycleTest::~MotorcycleTest()
+{
+	mPhysicsSystem->RemoveStepListener(mVehicleConstraint);
+}
+
+void MotorcycleTest::Initialize()
+{
+	VehicleTest::Initialize();
+
+	const float back_wheel_radius = 0.45f;
+	const float back_wheel_width = 0.06f;
+	const float back_wheel_pos_z = -0.75f;
+	const float back_suspension_min_length = 0.3f;
+	const float back_suspension_max_length = 0.5f;
+
+	const float front_wheel_radius = 0.53f;
+	const float front_wheel_width = 0.04f;
+	const float front_wheel_pos_z = 0.75f;
+	const float front_suspension_min_length = 0.3f;
+	const float front_suspension_max_length = 0.5f;
+
+	const float half_vehicle_length = 1.1f;
+	const float half_vehicle_width = 0.2f;
+	const float half_vehicle_height = 0.4f;
+
+	const float max_steering_angle = DegreesToRadians(30);
+
+	const float rake = DegreesToRadians(30);
+
+	// Create vehicle body
+	RVec3 position(0, 2, 0);
+	RefConst<Shape> motorcycle_shape = OffsetCenterOfMassShapeSettings(Vec3(0, -half_vehicle_height, 0), new BoxShape(Vec3(half_vehicle_width, half_vehicle_height, half_vehicle_length))).Create().Get();
+	BodyCreationSettings motorcycle_body_settings(motorcycle_shape, position, Quat::sIdentity(), EMotionType::Dynamic, Layers::MOVING);
+	motorcycle_body_settings.mOverrideMassProperties = EOverrideMassProperties::CalculateInertia;
+	motorcycle_body_settings.mMassPropertiesOverride.mMass = 1500.0f;
+	mMotorcycleBody = mBodyInterface->CreateBody(motorcycle_body_settings);
+	mBodyInterface->AddBody(mMotorcycleBody->GetID(), EActivation::Activate);
+
+	// Create vehicle constraint
+	VehicleConstraintSettings vehicle;
+	vehicle.mDrawConstraintSize = 0.1f;
+	vehicle.mMaxPitchRollAngle = DegreesToRadians(15.0f);
+
+	// Wheels
+	WheelSettingsWV *front = new WheelSettingsWV;
+	front->mPosition = Vec3(0.0f, -0.9f * half_vehicle_height, front_wheel_pos_z);
+	front->mMaxSteerAngle = max_steering_angle;
+	front->mDirection = Vec3(0, -1, Tan(rake)).Normalized();
+	front->mRadius = front_wheel_radius;
+	front->mWidth = front_wheel_width;
+	front->mSuspensionMinLength = front_suspension_min_length;
+	front->mSuspensionMaxLength = front_suspension_max_length;
+
+	WheelSettingsWV *back = new WheelSettingsWV;
+	back->mPosition = Vec3(0.0f, -0.9f * half_vehicle_height, back_wheel_pos_z);
+	back->mMaxSteerAngle = 0.0f;
+	back->mRadius = back_wheel_radius;
+	back->mWidth = back_wheel_width;
+	back->mSuspensionMinLength = back_suspension_min_length;
+	back->mSuspensionMaxLength = back_suspension_max_length;
+
+	vehicle.mWheels = { front, back };
+
+	WheeledVehicleControllerSettings *controller = new WheeledVehicleControllerSettings;
+	vehicle.mController = controller;
+
+	// Differential (not really applicable to a motorcycle but we need one anyway to drive it)
+	controller->mDifferentials.resize(1);
+	controller->mDifferentials[0].mLeftWheel = -1;
+	controller->mDifferentials[0].mRightWheel = 1;
+
+	mVehicleConstraint = new VehicleConstraint(*mMotorcycleBody, vehicle);
+	mVehicleConstraint->SetVehicleCollisionTester(new VehicleCollisionTesterCastSphere(Layers::MOVING, 0.5f * front_wheel_width));
+	mPhysicsSystem->AddConstraint(mVehicleConstraint);
+	mPhysicsSystem->AddStepListener(mVehicleConstraint);
+}
+
+void MotorcycleTest::PrePhysicsUpdate(const PreUpdateParams &inParams)
+{
+	// Determine acceleration and brake
+	float forward = 0.0f, right = 0.0f, brake = 0.0f;
+	if (inParams.mKeyboard->IsKeyPressed(DIK_UP))
+		forward = 1.0f;
+	else if (inParams.mKeyboard->IsKeyPressed(DIK_DOWN))		
+		forward = -1.0f;
+
+	// Check if we're reversing direction
+	if (mPreviousForward * forward < 0.0f)
+	{
+		// Get vehicle velocity in local space to the body of the vehicle
+		float velocity = (mMotorcycleBody->GetRotation().Conjugated() * mMotorcycleBody->GetLinearVelocity()).GetZ();
+		if ((forward > 0.0f && velocity < -0.1f) || (forward < 0.0f && velocity > 0.1f))
+		{
+			// Brake while we've not stopped yet
+			forward = 0.0f;
+			brake = 1.0f;
+		}
+		else
+		{
+			// When we've come to a stop, accept the new direction
+			mPreviousForward = forward;
+		}
+	}
+
+	// Steering
+	if (inParams.mKeyboard->IsKeyPressed(DIK_LEFT))
+		right = -1.0f;
+	else if (inParams.mKeyboard->IsKeyPressed(DIK_RIGHT))
+		right = 1.0f;
+
+	// On user input, assure that the motorcycle is active
+	if (right != 0.0f || forward != 0.0f || brake != 0.0f)
+		mBodyInterface->ActivateBody(mMotorcycleBody->GetID());
+
+	// Pass the input on to the constraint
+	WheeledVehicleController *controller = static_cast<WheeledVehicleController *>(mVehicleConstraint->GetController());
+	controller->SetDriverInput(forward, right, brake, false);
+
+	// Draw our wheels (this needs to be done in the pre update since we draw the bodies too in the state before the step)
+	for (uint w = 0; w < 2; ++w)
+	{
+		const WheelSettings *settings = mVehicleConstraint->GetWheels()[w]->GetSettings();
+		RMat44 wheel_transform = mVehicleConstraint->GetWheelWorldTransform(w, Vec3::sAxisY(), Vec3::sAxisX()); // The cyclinder we draw is aligned with Y so we specify that as rotational axis
+		mDebugRenderer->DrawCylinder(wheel_transform, 0.5f * settings->mWidth, settings->mRadius, Color::sGreen);
+	}
+}
+
+void MotorcycleTest::GetInitialCamera(CameraState &ioState) const 
+{
+	// Position camera behind motorcycle
+	RVec3 cam_tgt = RVec3(0, 0, 5);
+	ioState.mPos = RVec3(0, 2.5f, -5);
+	ioState.mForward = Vec3(cam_tgt - ioState.mPos).Normalized();
+}
+
+RMat44 MotorcycleTest::GetCameraPivot(float inCameraHeading, float inCameraPitch) const 
+{
+	// Pivot is center of motorcycle and rotates with motorcycle around Y axis only
+	Vec3 fwd = mMotorcycleBody->GetRotation().RotateAxisZ();
+	fwd.SetY(0.0f);
+	float len = fwd.Length();
+	if (len != 0.0f)
+		fwd /= len;
+	else
+		fwd = Vec3::sAxisZ();
+	Vec3 up = Vec3::sAxisY();
+	Vec3 right = up.Cross(fwd);
+	return RMat44(Vec4(right, 0), Vec4(up, 0), Vec4(fwd, 0), mMotorcycleBody->GetPosition());
+}
