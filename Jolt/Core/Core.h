ddd5b5e36d247115367cbf5544f59ae732cc95e9
Jorrit Rouwe <jrouwe@gmail.com>
2023-06-07 07:09:40 +0200

    uint*_t should be prefixed by std:: (#558)
    Fixes #557

--- Jolt/Core/Core.h
+++ Jolt/Core/Core.h
@@ -341,10 +341,10 @@ using std::istream;
 
 // Standard types
 using uint = unsigned int;
-using uint8 = uint8_t;
-using uint16 = uint16_t;
-using uint32 = uint32_t;
-using uint64 = uint64_t;
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
 
 // Assert sizes of types
 static_assert(sizeof(uint) >= 4, "Invalid size of uint");
