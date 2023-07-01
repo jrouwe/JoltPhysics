312c25a543bca3d8ee6532861d50f5b4df4a50cd
Jorrit Rouwe <jrouwe@gmail.com>
2023-07-01 17:12:03 +0200

    Temp solution that kind of works

--- Jolt/Physics/Body/MotionProperties.cpp
+++ Jolt/Physics/Body/MotionProperties.cpp
@@ -60,8 +60,8 @@ void MotionProperties::SetMassProperties(ELockedAxis inLockedAxis, const MassPro
 		if (num_locked_rotation_axis == 2)
 		{
 			// We can only rotate around one axis so the inverse inertia is trivial to calculate
-			mInvInertiaDiagonal = Vec3::sZero();
 			mInertiaRotation = Quat::sIdentity();
+			mInvInertiaDiagonal = Vec3::sZero();
 			for (int axis = 0; axis < 3; ++axis)
 				if ((locked_rotation_axis & (1 << axis)) == 0)
 					mInvInertiaDiagonal.SetComponent(axis, 1.0f / inMassProperties.mInertia(axis, axis));
@@ -71,35 +71,19 @@ void MotionProperties::SetMassProperties(ELockedAxis inLockedAxis, const MassPro
 			JPH_ASSERT(num_locked_rotation_axis == 1);
 			uint locked_axis = CountTrailingZeros(locked_rotation_axis);
 
+			// Get inverse inertia matrix and set locked axis elements to 0
 			Mat44 inverse_inertia = inMassProperties.mInertia.Inversed3x3();
-
-			// Get the 2 non-locked axis
-			Vec3 normalized_axis[2];
-			float axis_len[2];
-			uint next_axis = 0;
 			for (uint axis = 0; axis < 3; ++axis)
-				if (axis != locked_axis)
-				{
-					Vec3 &dest = normalized_axis[next_axis];
-					dest = inverse_inertia.GetColumn3(axis);
-					dest.SetComponent(locked_axis, 0.0f);
-					float dest_len = dest.Length();
-					dest /= dest_len;
-					axis_len[next_axis] = dest_len;
-					++next_axis;
-				}
-			JPH_ASSERT(next_axis == 2);
-
-			// Make sure that axis 0 is the smallest (largest inertia)
-			if (axis_len[0] > axis_len[1])
 			{
-				swap(normalized_axis[0], normalized_axis[1]);
-				swap(axis_len[0], axis_len[1]);
+				inverse_inertia(axis, locked_axis) = 0.0f;
+				inverse_inertia(locked_axis, axis) = 0.0f;
 			}
 
-			// Build a new inverse inertia matrix and decompose it
-			Mat44 reordered_inverse_inertia(Vec4(normalized_axis[0]), Vec4(normalized_axis[1]), Vec4(normalized_axis[0].Cross(normalized_axis[1])), Vec4(0, 0, 0, 1));
-			mInertiaRotation = reordered_inverse_inertia.Decompose(mInvInertiaDiagonal).GetQuaternion();
+			// Set the 2 components that are non zero
+			mInertiaRotation = Quat::sIdentity();
+			mInvInertiaDiagonal = Vec3::sZero();
+			for (int axis = 0; axis < 3; ++axis)
+				mInvInertiaDiagonal.SetComponent(axis, inverse_inertia.GetColumn3(axis).Length());
 		}
 	}
 
