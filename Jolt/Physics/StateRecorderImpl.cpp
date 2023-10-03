cdf6d671c3dbc67e186529fafaa1a116b7a91572
Jorrit Rouwe <jrouwe@gmail.com>
2023-10-03 18:03:52 +0200

    Formatting

--- Jolt/Physics/StateRecorderImpl.cpp
+++ Jolt/Physics/StateRecorderImpl.cpp
@@ -18,9 +18,10 @@ void StateRecorderImpl::Rewind()
 	mStream.seekg(0, std::stringstream::beg);
 }
 
-void StateRecorderImpl::Clear() {
+void StateRecorderImpl::Clear()
+{
 	mStream.clear();
-	mStream.str ({});
+	mStream.str({});
 }
 
 void StateRecorderImpl::ReadBytes(void *outData, size_t inNumBytes)
