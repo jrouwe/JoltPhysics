98f96a77bd0b1e905b87e07f7f086e78045c2581
Jorrit Rouwe <jrouwe@gmail.com>
2023-12-18 14:22:16 +0100

    SwingTwst / SixDOF constraints can now have asymmetric twist limits

--- Jolt/Physics/Constraints/SwingTwistConstraint.cpp
+++ Jolt/Physics/Constraints/SwingTwistConstraint.cpp
@@ -182,10 +182,10 @@ void SwingTwistConstraint::SetTargetOrientationCS(QuatArg inOrientation)
 	Quat q_swing, q_twist;
 	inOrientation.GetSwingTwist(q_swing, q_twist);
 
-	bool swing_y_clamped, swing_z_clamped, twist_clamped;
-	mSwingTwistConstraintPart.ClampSwingTwist(q_swing, swing_y_clamped, swing_z_clamped, q_twist, twist_clamped);
+	bool swing_y_clamped, swing_z_clamped, twist_clamped_to_min, twist_clamped_to_max;
+	mSwingTwistConstraintPart.ClampSwingTwist(q_swing, swing_y_clamped, swing_z_clamped, q_twist, twist_clamped_to_min, twist_clamped_to_max);
 
-	if (swing_y_clamped || swing_z_clamped || twist_clamped)
+	if (swing_y_clamped || swing_z_clamped || twist_clamped_to_min || twist_clamped_to_max)
 		mTargetOrientation = q_swing * q_twist;
 	else
 		mTargetOrientation = inOrientation;
