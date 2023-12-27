1efff7deda6ac461b2e2551531429700a0f1dd0a
Jorrit Rouwe <jrouwe@gmail.com>
2023-12-27 20:09:23 +0100

    Formatting & moving #include out of JPH namespace

--- Jolt/Core/JobSystemThreadPool.cpp
+++ Jolt/Core/JobSystemThreadPool.cpp
@@ -20,6 +20,9 @@
 
 	JPH_SUPPRESS_WARNING_POP
 #endif
+#ifdef JPH_PLATFORM_LINUX
+	#include <sys/prctl.h>
+#endif
 
 JPH_NAMESPACE_BEGIN
 
@@ -231,45 +234,43 @@ void JobSystemThreadPool::QueueJobs(Job **inJobs, uint inNumJobs)
 }
 
 #if defined(JPH_PLATFORM_WINDOWS) && !defined(JPH_COMPILER_MINGW) // MinGW doesn't support __try/__except
-
-// Sets the current thread name in MSVC debugger
-static void SetThreadName(const char *inName)
-{
-	#pragma pack(push, 8)
-
-	struct THREADNAME_INFO
-	{
-		DWORD	dwType;			// Must be 0x1000.
-		LPCSTR	szName;			// Pointer to name (in user addr space).
-		DWORD	dwThreadID;		// Thread ID (-1=caller thread).
-		DWORD	dwFlags;		// Reserved for future use, must be zero.
-	};
-
-	#pragma pack(pop)
-
-	THREADNAME_INFO info;
-	info.dwType = 0x1000;
-	info.szName = inName;
-	info.dwThreadID = (DWORD)-1;
-	info.dwFlags = 0;
-
-	__try
-	{
-		RaiseException(0x406D1388, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR *)&info);
-	}
-	__except(EXCEPTION_EXECUTE_HANDLER)
-	{
-	}
-}
-#elif defined(JPH_PLATFORM_LINUX)
-	#include <sys/prctl.h>
+	// Sets the current thread name in MSVC debugger
 	static void SetThreadName(const char *inName)
 	{
-		char truncatedName[16] = {0};
-		strncpy(truncatedName, inName, std::min(sizeof(truncatedName), 15ul));
-		prctl(PR_SET_NAME, truncatedName, 0, 0, 0);
+		#pragma pack(push, 8)
+
+		struct THREADNAME_INFO
+		{
+			DWORD	dwType;			// Must be 0x1000.
+			LPCSTR	szName;			// Pointer to name (in user addr space).
+			DWORD	dwThreadID;		// Thread ID (-1=caller thread).
+			DWORD	dwFlags;		// Reserved for future use, must be zero.
+		};
+
+		#pragma pack(pop)
+
+		THREADNAME_INFO info;
+		info.dwType = 0x1000;
+		info.szName = inName;
+		info.dwThreadID = (DWORD)-1;
+		info.dwFlags = 0;
+
+		__try
+		{
+			RaiseException(0x406D1388, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR *)&info);
+		}
+		__except(EXCEPTION_EXECUTE_HANDLER)
+		{
+		}
 	}
-#endif // JPH_PLATFORM_WINDOWS && !JPH_COMPILER_MINGW
+#elif defined(JPH_PLATFORM_LINUX)
+	static void SetThreadName(const char *inName)
+	{
+		char truncated_name[16] = { 0 };
+		strncpy(truncated_name, inName, min(sizeof(truncated_name), 15ul));
+		prctl(PR_SET_NAME, truncated_name, 0, 0, 0);
+	}
+#endif // JPH_PLATFORM_LINUX
 
 void JobSystemThreadPool::ThreadMain(int inThreadIndex)
 {
@@ -277,7 +278,7 @@ void JobSystemThreadPool::ThreadMain(int inThreadIndex)
 	char name[64];
 	snprintf(name, sizeof(name), "Worker %d", int(inThreadIndex + 1));
 
-#if defined(JPH_PLATFORM_WINDOWS) && !defined(JPH_COMPILER_MINGW) || defined(JPH_PLATFORM_LINUX)
+#if (defined(JPH_PLATFORM_WINDOWS) && !defined(JPH_COMPILER_MINGW)) || defined(JPH_PLATFORM_LINUX)
 	SetThreadName(name);
 #endif // JPH_PLATFORM_WINDOWS && !JPH_COMPILER_MINGW
 
