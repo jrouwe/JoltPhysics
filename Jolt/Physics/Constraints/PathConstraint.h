dfea2920634ecdbc14455710e6ca36e6876a34de
Jorrit Rouwe <jrouwe@gmail.com>
2024-03-07 22:15:56 +0100

    Fixed whitespace

--- Jolt/Physics/Constraints/PathConstraint.h
+++ Jolt/Physics/Constraints/PathConstraint.h
@@ -31,7 +31,7 @@ enum class EPathRotationConstraintType
 /// * Tangent, normal and bi-normal form an orthonormal basis with: tangent cross bi-normal = normal
 /// * The path points along the tangent vector
 /// * The path is continuous so doesn't contain any sharp corners
-/// 
+///
 /// The reason for all this is that the constraint acts like a slider constraint with the sliding axis being the tangent vector (the assumption here is that delta time will be small enough so that the path is linear for that delta time).
 class JPH_EXPORT PathConstraintSettings final : public TwoBodyConstraintSettings
 {
