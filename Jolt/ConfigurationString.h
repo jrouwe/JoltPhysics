40dc67068b853761baf262cc47e3cf567e2118e9
Andrea Catania <info@andreacatania.com>
2024-04-28 20:29:37 +0200

    Extended GetConfigurationString with state of more preprocessor symbols (#1074)

--- Jolt/ConfigurationString.h
+++ Jolt/ConfigurationString.h
@@ -62,6 +62,20 @@ inline const char *GetConfigurationString()
 #ifdef JPH_FLOATING_POINT_EXCEPTIONS_ENABLED
 		"(FP Exceptions) "
 #endif
+#ifdef JPH_DEBUG_RENDERER
+		"(Debug Renderer) "
+#endif
+#ifdef JPH_PROFILE_ENABLED
+		"(Profile) "
+#endif
+#if defined(JPH_OBJECT_LAYER_BITS) && JPH_OBJECT_LAYER_BITS == 32
+		"(32-bit ObjectLayer) "
+#else
+		"(16-bit ObjectLayer) "
+#endif
+#ifdef JPH_ENABLE_ASSERTS
+		"(Assertions) "
+#endif
 #ifdef JPH_DEBUG
 		"(Debug) "
 #endif
