0a91274da459cbd42433681813c59ad65dbf9c3f
Jorrit Rouwe <jrouwe@gmail.com>
2024-05-03 14:08:46 +0200

    Shuffled code around

--- Jolt/Core/Array.h
+++ Jolt/Core/Array.h
@@ -45,68 +45,157 @@ public:
 	using const_iterator = const T *;
 	using iterator = T *;
 
-	/// Default constructor
-						Array() = default;
-
-	/// Constructor with allocator
-	explicit			Array(const Allocator &inAllocator) :
-		Allocator(inAllocator)
+private:
+	/// Grow the array by at least inAmount elements
+	inline void				grow(size_type inAmount = 1)
 	{
+		size_type min_size = mSize + inAmount;
+		if (min_size > mCapacity)
+		{
+			size_type new_capacity = max(min_size, mCapacity * 2);
+			reserve(new_capacity);
+		}
 	}
 
-	/// Constructor with length
-	explicit			Array(size_type inLength, const Allocator &inAllocator = { }) :
-		Allocator(inAllocator)
+	/// Destroy all elements and free memory
+	inline void				destroy()
 	{
-		resize(inLength);
+		if (mElements != nullptr)
+		{
+			clear();
+
+			get_allocator().deallocate(mElements, mCapacity);
+			mElements = nullptr;
+			mCapacity = 0;
+		}
 	}
 
-	/// Constructor with length and value
-						Array(size_type inLength, const T &inValue, const Allocator &inAllocator = { }) :
-		Allocator(inAllocator)
+public:
+	/// Reserve array space
+	inline void				reserve(size_type inNewSize)
 	{
-		reserve(inLength);
+		if (mCapacity < inNewSize)
+		{
+			pointer pointer = get_allocator().allocate(inNewSize);
+			if (mElements != nullptr)
+			{
+				memcpy(pointer, mElements, mSize * sizeof(T));
+				get_allocator().deallocate(mElements, mCapacity);
+			}
+			mElements = pointer;
+			mCapacity = inNewSize;
+		}
+	}
 
-		for (T *element = mElements, *element_end = mElements + inLength; element < element_end; ++element)
+	/// Resize array to new length
+	inline void				resize(size_type inNewSize)
+	{
+		if constexpr (!is_trivially_destructible<T>())
+			for (T *element = mElements + inNewSize, *element_end = mElements + mSize; element < element_end; ++element)
+				element->~T();
+
+		reserve(inNewSize);
+
+		if constexpr (!is_trivially_constructible<T>())
+			for (T *element = mElements + mSize, *element_end = mElements + inNewSize; element < element_end; ++element)
+				::new (element) T;
+		mSize = inNewSize;
+	}
+
+	/// Resize array to new length and initialize all elements with inValue
+	inline void				resize(size_type inNewSize, const T &inValue)
+	{
+		JPH_ASSERT(&inValue < mElements || &inValue >= mElements + mSize, "Can't pass an element from the array to resize");
+
+		if constexpr (!is_trivially_destructible<T>())
+			for (T *element = mElements + inNewSize, *element_end = mElements + mSize; element < element_end; ++element)
+				element->~T();
+
+		reserve(inNewSize);
+
+		for (T *element = mElements + mSize, *element_end = mElements + inNewSize; element < element_end; ++element)
 			::new (element) T(inValue);
-		mSize = inLength;
+		mSize = inNewSize;
 	}
 
-	/// Constructor from initializer list
-						Array(std::initializer_list<T> inList, const Allocator &inAllocator = { }) :
-		Allocator(inAllocator)
+	/// Destruct all elements and set length to zero
+	inline void				clear()
 	{
+		if constexpr (!is_trivially_destructible<T>())
+			for (T *element = mElements, *end = element + mSize; element < end; ++element)
+				element->~T();
+		mSize = 0;
+	}
+
+	/// Replace the contents of this array with inBegin .. inEnd
+	template <class Iterator>
+	inline void				assign(Iterator inBegin, Iterator inEnd)
+	{
+		clear();
+
+		reserve(size_type(std::distance(inBegin, inEnd)));
+
+		for (Iterator element = inBegin; element != inEnd; ++element)
+			::new (&mElements[mSize++]) T(*element);
+	}
+
+	/// Replace the contents of this array with inList
+	inline void				assign(std::initializer_list<T> inList)
+	{
+		clear();
+
 		reserve(size_type(inList.size()));
 
 		for (typename std::initializer_list<T>::iterator i = inList.begin(); i != inList.end(); ++i)
 			::new (&mElements[mSize++]) T(*i);
 	}
 
-	/// Constructor from iterator
-						Array(const_iterator inBegin, const_iterator inEnd, const Allocator &inAllocator = { }) :
+	/// Default constructor
+							Array() = default;
+
+	/// Constructor with allocator
+	explicit inline			Array(const Allocator &inAllocator) :
 		Allocator(inAllocator)
 	{
-		reserve(size_type(inEnd - inBegin));
+	}
 
-		for (const_iterator element = inBegin; element < inEnd; ++element)
-			::new (&mElements[mSize++]) T(*element);
+	/// Constructor with length
+	explicit inline			Array(size_type inLength, const Allocator &inAllocator = { }) :
+		Allocator(inAllocator)
+	{
+		resize(inLength);
+	}
+
+	/// Constructor with length and value
+	inline					Array(size_type inLength, const T &inValue, const Allocator &inAllocator = { }) :
+		Allocator(inAllocator)
+	{
+		resize(inLength, inValue);
+	}
+
+	/// Constructor from initializer list
+	inline					Array(std::initializer_list<T> inList, const Allocator &inAllocator = { }) :
+		Allocator(inAllocator)
+	{
+		assign(inList);
+	}
+
+	/// Constructor from iterator
+	inline					Array(const_iterator inBegin, const_iterator inEnd, const Allocator &inAllocator = { }) :
+		Allocator(inAllocator)
+	{
+		assign(inBegin, inEnd);
 	}
 
 	/// Copy constructor
-						Array(const Array<T, Allocator> &inRHS) :
+	inline					Array(const Array<T, Allocator> &inRHS) :
 		Allocator(inRHS.get_allocator())
 	{
-		reserve(inRHS.mSize);
-
-		while (mSize < inRHS.mSize)
-		{
-			::new (&mElements[mSize]) T(inRHS[mSize]);
-			++mSize;
-		}
+		assign(inRHS.begin(), inRHS.end());
 	}
 
 	/// Move constructor
-						Array(Array<T, Allocator> &&inRHS)
+	inline					Array(Array<T, Allocator> &&inRHS)
 	{
 		destroy();
 
@@ -123,33 +212,24 @@ public:
 	}
 
 	/// Destruct all elements
-						~Array()
+	inline					~Array()
 	{
 		destroy();
 	}
 
 	/// Get the allocator
-	Allocator &			get_allocator()
+	inline Allocator &		get_allocator()
 	{
 		return *this;
 	}
 
-	const Allocator &	get_allocator() const
+	inline const Allocator &get_allocator() const
 	{
 		return *this;
-	}
-
-	/// Destruct all elements and set length to zero
-	void				clear()
-	{
-		if constexpr (!is_trivially_destructible<T>())
-			for (T *element = mElements, *end = element + mSize; element < end; ++element)
-				element->~T();
-		mSize = 0;
 	}
 
 	/// Add element to the back of the array
-	void				push_back(const T &inValue)
+	inline void				push_back(const T &inValue)
 	{
 		JPH_ASSERT(&inValue < mElements || &inValue >= mElements + mSize, "Can't pass an element from the array to push_back");
 
@@ -159,8 +239,10 @@ public:
 		::new (element) T(inValue);
 	}
 
-	void				push_back(T &&inValue)
+	inline void				push_back(T &&inValue)
 	{
+		JPH_ASSERT(&inValue < mElements || &inValue >= mElements + mSize, "Can't pass an element from the array to push_back");
+
 		grow();
 
 		T *element = mElements + mSize++;
@@ -169,7 +251,7 @@ public:
 
 	/// Construct element at the back of the array
 	template <class... A>
-	T &					emplace_back(A &&... inValue)
+	inline T &				emplace_back(A &&... inValue)
 	{
 		grow();
 
@@ -179,79 +261,32 @@ public:
 	}
 
 	/// Remove element from the back of the array
-	void				pop_back()
+	inline void				pop_back()
 	{
 		JPH_ASSERT(mSize > 0);
 		mElements[--mSize].~T();
 	}
 
 	/// Returns true if there are no elements in the array
-	bool				empty() const
+	inline bool				empty() const
 	{
 		return mSize == 0;
 	}
 
 	/// Returns amount of elements in the array
-	size_type			size() const
+	inline size_type		size() const
 	{
 		return mSize;
 	}
 
 	/// Returns maximum amount of elements the array can hold
-	size_type			capacity() const
+	inline size_type		capacity() const
 	{
 		return mCapacity;
 	}
 
-	/// Reserve array space
-	void				reserve(size_type inNewSize)
-	{
-		if (mCapacity < inNewSize)
-		{
-			pointer pointer = get_allocator().allocate(inNewSize);
-			if (mElements != nullptr)
-			{
-				memcpy(pointer, mElements, mSize * sizeof(T));
-				get_allocator().deallocate(mElements, mCapacity);
-			}
-			mElements = pointer;
-			mCapacity = inNewSize;
-		}
-	}
-
-	/// Resize array to new length
-	void				resize(size_type inNewSize)
-	{
-		if constexpr (!is_trivially_destructible<T>())
-			for (T *element = mElements + inNewSize, *element_end = mElements + mSize; element < element_end; ++element)
-				element->~T();
-
-		reserve(inNewSize);
-
-		if constexpr (!is_trivially_constructible<T>())
-			for (T *element = mElements + mSize, *element_end = mElements + inNewSize; element < element_end; ++element)
-				::new (element) T;
-		mSize = inNewSize;
-	}
-
-	/// Resize array to new length and initialize all elements with inValue
-	void				resize(size_type inNewSize, const T &inValue)
-	{
-		JPH_ASSERT(&inValue < mElements || &inValue >= mElements + mSize, "Can't pass an element from the array to resize");
-
-		if constexpr (!is_trivially_destructible<T>())
-			for (T *element = mElements + inNewSize, *element_end = mElements + mSize; element < element_end; ++element)
-				element->~T();
-
-		reserve(inNewSize);
-
-		for (T *element = mElements + mSize, *element_end = mElements + inNewSize; element < element_end; ++element)
-			::new (element) T(inValue);
-		mSize = inNewSize;
-	}
-
 	/// Reduce the capacity of the array to match its size
-	void				shrink_to_fit()
+	void					shrink_to_fit()
 	{
 		if (mCapacity > mSize)
 		{
@@ -263,31 +298,8 @@ public:
 		}
 	}
 
-	/// Replace the contents of this array with inBegin .. inEnd
-	template <class Iterator>
-	void				assign(Iterator inBegin, Iterator inEnd)
-	{
-		clear();
-
-		reserve(size_type(std::distance(inBegin, inEnd)));
-
-		for (Iterator element = inBegin; element != inEnd; ++element)
-			::new (&mElements[mSize++]) T(*element);
-	}
-
-	/// Replace the contents of this array with inList
-	void				assign(std::initializer_list<T> inList)
-	{
-		clear();
-
-		reserve(size_type(inList.size()));
-
-		for (typename std::initializer_list<T>::iterator i = inList.begin(); i != inList.end(); ++i)
-			::new (&mElements[mSize++]) T(*i);
-	}
-
 	/// Swap the contents of two arrays
-	void				swap(Array<T, Allocator> &inRHS)
+	void					swap(Array<T, Allocator> &inRHS)
 	{
 		std::swap(get_allocator(), inRHS.get_allocator());
 		std::swap(mSize, inRHS.mSize);
@@ -296,7 +308,7 @@ public:
 	}
 
 	template <class Iterator>
-	void				insert(const_iterator inPos, Iterator inBegin, Iterator inEnd)
+	void					insert(const_iterator inPos, Iterator inBegin, Iterator inEnd)
 	{
 		// After grow() inPos may be invalid
 		size_type first_element = inPos - mElements;
@@ -314,7 +326,7 @@ public:
 		mSize += num_elements;
 	}
 
-	void				insert(const_iterator inPos, const T &inValue)
+	void					insert(const_iterator inPos, const T &inValue)
 	{
 		JPH_ASSERT(&inValue < mElements || &inValue >= mElements + mSize, "Can't pass an element from the array to insert");
 
@@ -330,101 +342,8 @@ public:
 		mSize++;
 	}
 
-	/// Iterators
-	const_iterator		begin() const
-	{
-		return mElements;
-	}
-
-	const_iterator		end() const
-	{
-		return mElements + mSize;
-	}
-
-	const_iterator		cbegin() const
-	{
-		return mElements;
-	}
-
-	const_iterator		cend() const
-	{
-		return mElements + mSize;
-	}
-
-	iterator			begin()
-	{
-		return mElements;
-	}
-
-	iterator			end()
-	{
-		return mElements + mSize;
-	}
-
-	const T *			data() const
-	{
-		return mElements;
-	}
-
-	T *					data()
-	{
-		return mElements;
-	}
-
-	/// Access element
-	T &					operator [] (size_type inIdx)
-	{
-		JPH_ASSERT(inIdx < mSize);
-		return mElements[inIdx];
-	}
-
-	const T &			operator [] (size_type inIdx) const
-	{
-		JPH_ASSERT(inIdx < mSize);
-		return mElements[inIdx];
-	}
-
-	/// Access element
-	T &					at(size_type inIdx)
-	{
-		JPH_ASSERT(inIdx < mSize);
-		return mElements[inIdx];
-	}
-
-	const T &			at(size_type inIdx) const
-	{
-		JPH_ASSERT(inIdx < mSize);
-		return mElements[inIdx];
-	}
-
-	/// First element in the array
-	const T &			front() const
-	{
-		JPH_ASSERT(mSize > 0);
-		return mElements[0];
-	}
-
-	T &					front()
-	{
-		JPH_ASSERT(mSize > 0);
-		return mElements[0];
-	}
-
-	/// Last element in the array
-	const T &			back() const
-	{
-		JPH_ASSERT(mSize > 0);
-		return mElements[mSize - 1];
-	}
-
-	T &					back()
-	{
-		JPH_ASSERT(mSize > 0);
-		return mElements[mSize - 1];
-	}
-
 	/// Remove one element from the array
-	void				erase(const_iterator inIter)
+	void					erase(const_iterator inIter)
 	{
 		size_type p = size_type(inIter - begin());
 		JPH_ASSERT(p < mSize);
@@ -435,7 +354,7 @@ public:
 	}
 
 	/// Remove multiple element from the array
-	void				erase(const_iterator inBegin, const_iterator inEnd)
+	void					erase(const_iterator inBegin, const_iterator inEnd)
 	{
 		size_type p = size_type(inBegin - begin());
 		size_type n = size_type(inEnd - inBegin);
@@ -445,6 +364,99 @@ public:
 		if (p + n < mSize)
 			memmove(mElements + p, mElements + p + n, (mSize - p - n) * sizeof(T));
 		mSize -= n;
+	}
+
+	/// Iterators
+	inline const_iterator	begin() const
+	{
+		return mElements;
+	}
+
+	inline const_iterator	end() const
+	{
+		return mElements + mSize;
+	}
+
+	inline const_iterator	cbegin() const
+	{
+		return mElements;
+	}
+
+	inline const_iterator	cend() const
+	{
+		return mElements + mSize;
+	}
+
+	inline iterator			begin()
+	{
+		return mElements;
+	}
+
+	inline iterator			end()
+	{
+		return mElements + mSize;
+	}
+
+	inline const T *		data() const
+	{
+		return mElements;
+	}
+
+	inline T *				data()
+	{
+		return mElements;
+	}
+
+	/// Access element
+	inline T &				operator [] (size_type inIdx)
+	{
+		JPH_ASSERT(inIdx < mSize);
+		return mElements[inIdx];
+	}
+
+	inline const T &		operator [] (size_type inIdx) const
+	{
+		JPH_ASSERT(inIdx < mSize);
+		return mElements[inIdx];
+	}
+
+	/// Access element
+	inline T &				at(size_type inIdx)
+	{
+		JPH_ASSERT(inIdx < mSize);
+		return mElements[inIdx];
+	}
+
+	inline const T &		at(size_type inIdx) const
+	{
+		JPH_ASSERT(inIdx < mSize);
+		return mElements[inIdx];
+	}
+
+	/// First element in the array
+	inline const T &		front() const
+	{
+		JPH_ASSERT(mSize > 0);
+		return mElements[0];
+	}
+
+	inline T &				front()
+	{
+		JPH_ASSERT(mSize > 0);
+		return mElements[0];
+	}
+
+	/// Last element in the array
+	inline const T &		back() const
+	{
+		JPH_ASSERT(mSize > 0);
+		return mElements[mSize - 1];
+	}
+
+	inline T &				back()
+	{
+		JPH_ASSERT(mSize > 0);
+		return mElements[mSize - 1];
 	}
 
 	/// Assignment operator
@@ -465,7 +477,7 @@ public:
 	}
 
 	/// Comparing arrays
-	bool				operator == (const Array<T, Allocator> &inRHS) const
+	bool					operator == (const Array<T, Allocator> &inRHS) const
 	{
 		if (mSize != inRHS.mSize)
 			return false;
@@ -475,7 +487,7 @@ public:
 		return true;
 	}
 
-	bool				operator != (const Array<T, Allocator> &inRHS) const
+	bool					operator != (const Array<T, Allocator> &inRHS) const
 	{
 		if (mSize != inRHS.mSize)
 			return true;
@@ -486,33 +498,9 @@ public:
 	}
 
 private:
-	/// Grow the array by at least inAmount elements
-	inline void			grow(size_type inAmount = 1)
-	{
-		size_type min_size = mSize + inAmount;
-		if (min_size > mCapacity)
-		{
-			size_type new_capacity = max(min_size, mCapacity * 2);
-			reserve(new_capacity);
-		}
-	}
-
-	/// Destroy all elements and free memory
-	inline void			destroy()
-	{
-		if (mElements != nullptr)
-		{
-			clear();
-
-			get_allocator().deallocate(mElements, mCapacity);
-			mElements = nullptr;
-			mCapacity = 0;
-		}
-	}
-
-	size_type			mSize = 0;
-	size_type			mCapacity = 0;
-	T *					mElements = nullptr;
+	size_type				mSize = 0;
+	size_type				mCapacity = 0;
+	T *						mElements = nullptr;
 };
 
 JPH_NAMESPACE_END
