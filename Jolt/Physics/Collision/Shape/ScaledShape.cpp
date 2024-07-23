99ea920b8e5c0df25a2fb7ab8343d49820bf81a6
Mikael Hermansson <mikael@hermansson.io>
2024-07-23 23:20:40 +0200

    Removed `MakeNonZeroScale` from `MakeScaleValid`

--- Jolt/Physics/Collision/Shape/ScaledShape.cpp
+++ Jolt/Physics/Collision/Shape/ScaledShape.cpp
@@ -178,9 +178,7 @@ bool ScaledShape::IsValidScale(Vec3Arg inScale) const
 
 Vec3 ScaledShape::MakeScaleValid(Vec3Arg inScale) const
 {
-	Vec3 scale = ScaleHelpers::MakeNonZeroScale(mScale);
-
-	return mInnerShape->MakeScaleValid(scale * inScale) / scale;
+	return mInnerShape->MakeScaleValid(mScale * inScale) / mScale;
 }
 
 void ScaledShape::sCollideScaledVsShape(const Shape *inShape1, const Shape *inShape2, Vec3Arg inScale1, Vec3Arg inScale2, Mat44Arg inCenterOfMassTransform1, Mat44Arg inCenterOfMassTransform2, const SubShapeIDCreator &inSubShapeIDCreator1, const SubShapeIDCreator &inSubShapeIDCreator2, const CollideShapeSettings &inCollideShapeSettings, CollideShapeCollector &ioCollector, const ShapeFilter &inShapeFilter)
