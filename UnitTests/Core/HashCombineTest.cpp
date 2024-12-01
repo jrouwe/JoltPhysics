7221323294f241f5266b1b94eafa2dc5e670c2ba
Jorrit Rouwe <jrouwe@gmail.com>
2024-12-01 14:42:56 +0100

    Faster hashing for IndexedTriangle to improve MeshShape::Sanitize (#1368)
    Also replaced HashCombine with a 64-bit version. Note that this version is slower but it's not used in any perf critical code.

--- UnitTests/Core/HashCombineTest.cpp
+++ UnitTests/Core/HashCombineTest.cpp
@@ -28,4 +28,24 @@ TEST_SUITE("HashCombineTest")
 		String str_test = "This is a test";
 		CHECK(Hash<String> { } (str_test) == 2733878766136413408UL);
 	}
+
+	TEST_CASE("TestHashCombine")
+	{
+		int val1 = 0;
+		uint64 val1_hash = Hash<int> { } (val1);
+		int val2 = 1;
+		uint64 val2_hash = Hash<int> { } (val2);
+
+		// Check non-commutative
+		uint64 seed1 = val1_hash;
+		HashCombine(seed1, val2);
+		uint64 seed2 = val2_hash;
+		HashCombine(seed2, val1);
+		CHECK(seed1 != seed2);
+
+		// Check that adding a 0 changes the hash
+		uint64 seed3 = val1_hash;
+		HashCombine(seed3, val1);
+		CHECK(seed3 != val1_hash);
+	}
 }
