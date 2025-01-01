8aa398ba116195b5c323921fd34f02f9a1ff5690
Jorrit Rouwe <jrouwe@gmail.com>
2025-01-01 18:13:11 +0100

    Add close buttons

--- TestFramework/Window/ApplicationWindowMacOS.mm
+++ TestFramework/Window/ApplicationWindowMacOS.mm
@@ -14,17 +14,20 @@ void ApplicationWindowMacOS::Initialize()
 	@autoreleasepool
 	{
 		[NSApplication sharedApplication];
-		[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-		mWindow = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, mWindowWidth, mWindowHeight) styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:NO];
-		[mWindow cascadeTopLeftFromPoint:NSMakePoint(20, 20)];
+		[NSApp setActivationPolicy: NSApplicationActivationPolicyRegular];
+		mWindow = [[NSWindow alloc] initWithContentRect: NSMakeRect(0, 0, mWindowWidth, mWindowHeight)
+											  styleMask: NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable | NSWindowStyleMaskMiniaturizable
+												backing: NSBackingStoreBuffered
+												  defer: NO];
+		[mWindow cascadeTopLeftFromPoint: NSMakePoint(20, 20)];
 		[mWindow setTitle: @"TestFramework"];
-		[mWindow makeKeyAndOrderFront:nil];
-		[NSApp activateIgnoringOtherApps:YES];
+		[mWindow makeKeyAndOrderFront: nil];
+		[NSApp activateIgnoringOtherApps: YES];
 		
 		NSView *content_view = [mWindow contentView];
 		mMetalLayer = [CAMetalLayer layer];
-		[content_view setLayer:mMetalLayer];
-		[content_view setWantsLayer:YES];
+		[content_view setLayer: mMetalLayer];
+		[content_view setWantsLayer: YES];
 		//		[NSApp run];
 	}
 }
@@ -35,15 +38,14 @@ void ApplicationWindowMacOS::MainLoop(RenderCallback inRenderCallback)
 	{
 		for (;;)
 		{
-//			if (![NSApp isRunning])
-//				return false;
+			// Process events
 			for (;;)
 			{
-				NSEvent* event = [NSApp nextEventMatchingMask:NSEventMaskAny untilDate:nil inMode:NSDefaultRunLoopMode dequeue:YES];
+				NSEvent* event = [NSApp nextEventMatchingMask: NSEventMaskAny untilDate: nil inMode: NSDefaultRunLoopMode dequeue: YES];
 				if (event == nullptr)
 					break;
-				[NSApp sendEvent:event];
-//				[NSApp updateWindows];
+				[NSApp sendEvent: event];
+				[NSApp updateWindows];
 			}
 
 			// Call the render callback
