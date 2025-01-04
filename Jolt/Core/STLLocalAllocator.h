e984ce85cfa5544dc3d479ae2c841ffe53b25a38
Jorrit Rouwe <jrouwe@gmail.com>
2025-01-04 12:13:41 +0100

    Fixed UBSAN warning: passing nullptr to memcpy

--- Jolt/Core/STLLocalAllocator.h
+++ Jolt/Core/STLLocalAllocator.h
@@ -61,18 +61,22 @@ public:
 	{
 		JPH_ASSERT(inNewSize > 0); // Reallocating to zero size is implementation dependent, so we don't allow it
 
+		// If there was no previous allocation, we can go through the regular allocate function
+		if (inOldPointer == nullptr)
+			return allocate(inNewSize);
+
 		// If the pointer is outside our local buffer, fall back to the heap
-		if (inOldPointer != nullptr && !is_local(inOldPointer))
+		if (!is_local(inOldPointer))
 		{
-			if constexpr (!AllocatorHasReallocate<Base>::sValue)
-				return ReallocateImpl(inOldPointer, inOldSize, inNewSize);
-			else
+			if constexpr (AllocatorHasReallocate<Base>::sValue)
 				return Base::reallocate(inOldPointer, inOldSize, inNewSize);
+			else
+				return ReallocateImpl(inOldPointer, inOldSize, inNewSize);
 		}
 
 		// If we happen to have space left, we only need to update our bookkeeping
 		pointer base_ptr = reinterpret_cast<pointer>(mElements) + mNumElementsUsed - inOldSize;
-		if ((inOldPointer == nullptr || inOldPointer == base_ptr)
+		if (inOldPointer == base_ptr
 			&& mNumElementsUsed - inOldSize + inNewSize <= N)
 		{
 			mNumElementsUsed += inNewSize - inOldSize;
@@ -151,7 +155,6 @@ private:
 
 /// The STLLocalAllocator always implements a reallocate function as it can often reallocate in place
 template <class T, size_t N> struct AllocatorHasReallocate<STLLocalAllocator<T, N>> { static constexpr bool sValue = STLLocalAllocator<T, N>::has_reallocate; };
-
 
 #else
 
