b62e994da4c135b8f197bb1e0e6dd62d1976b27d
Jorrit Rouwe <jrouwe@gmail.com>
2025-02-09 11:54:14 +0100

    No longer limiting max voided features/delayed results (#1500)
    Fixed InternalEdgeRemovingCollector not working when colliding with a very dense triangle grid because it ran out of internal space. Now falling back to memory allocations when this happens to avoid ghost collisions.
    
    See: godotengine/godot#102544

--- Jolt/Jolt.cmake
+++ Jolt/Jolt.cmake
@@ -644,7 +644,7 @@ else()
 		if (JPH_USE_WASM64)
 			target_compile_options(Jolt PUBLIC -sMEMORY64)
 			target_link_options(Jolt PUBLIC -sMEMORY64)
-		endif()		
+		endif()
 	elseif ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64" OR "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "AMD64" OR "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86" OR "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "i386")
 		# x86 and x86_64
 		# On 32-bit builds we need to default to using SSE instructions, the x87 FPU instructions have higher intermediate precision
