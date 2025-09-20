ed18a76edd186fc8375d53904ba8b5bf7568114f
Jorrit Rouwe <jrouwe@gmail.com>
2025-09-20 20:53:31 +0200

    Trying to fix Windows ARM 32 build

--- Build/cmake_vs2022_cl_arm_32bit.bat
+++ Build/cmake_vs2022_cl_arm_32bit.bat
@@ -1,3 +1,3 @@
 @echo off
-cmake -S . -B VS2022_CL_ARM_32BIT -G "Visual Studio 17 2022" -A ARM %*
+cmake -S . -B VS2022_CL_ARM_32BIT -G "Visual Studio 17 2022" -A ARM -DCMAKE_SYSTEM_VERSION="10.0.22621.0" -DCMAKE_CXX_COMPILER_WORKS=1 %*
 echo Open VS2022_CL_ARM_32BIT\JoltPhysics.sln to build the project.
