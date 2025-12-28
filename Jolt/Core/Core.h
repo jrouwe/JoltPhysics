a09409bf78490f6ac2be1b2308c9ef3d726722ad
Jorrit Rouwe <jrouwe@gmail.com>
2025-12-28 20:07:03 +0100

    Sonar fixes

--- Jolt/Core/Core.h
+++ Jolt/Core/Core.h
@@ -650,12 +650,12 @@ static_assert(sizeof(uint64) == 8, "Invalid size of uint64");
 #endif
 
 // DirectX 12 is only supported on Windows
-#ifndef JPH_PLATFORM_WINDOWS
+#if defined(JPH_USE_DX12) && !defined(JPH_PLATFORM_WINDOWS)
 	#undef JPH_USE_DX12
 #endif // JPH_PLATFORM_WINDOWS
 
 // Metal is only supported on Apple platforms
-#if !defined(JPH_PLATFORM_MACOS) && !defined(JPH_PLATFORM_IOS)
+#if defined(JPH_USE_METAL) && !defined(JPH_PLATFORM_MACOS) && !defined(JPH_PLATFORM_IOS)
 	#undef JPH_USE_METAL
 #endif // !JPH_PLATFORM_MACOS && !JPH_PLATFORM_IOS
 
