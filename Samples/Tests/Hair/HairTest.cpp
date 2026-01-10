985462d05ad2038ab2e894a752c97472089c4c60
Jorrit Rouwe <jrouwe@gmail.com>
2026-01-10 21:25:50 +0100

    Clamping sqrt to zero to prevent length of slightly over 1 resulting in a floating point crash
    [skip ci]

--- Samples/Tests/Hair/HairTest.cpp
+++ Samples/Tests/Hair/HairTest.cpp
@@ -93,7 +93,7 @@ void HairTest::Initialize()
 			Float3 translation, rotation;
 			stream.Read(translation);
 			stream.Read(rotation);
-			Quat rotation_quat(rotation.x, rotation.y, rotation.z, sqrt(1.0f - Vec3(rotation).LengthSq()));
+			Quat rotation_quat(rotation.x, rotation.y, rotation.z, sqrt(max(0.0f, 1.0f - Vec3(rotation).LengthSq())));
 			mFaceAnimation[frame][joint] = Mat44::sRotationTranslation(rotation_quat, Vec3(translation));
 		}
 	}
