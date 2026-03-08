0bf577e4204d653c789de9819954ada4b52ec1aa
Jorrit Rouwe <jrouwe@gmail.com>
2026-03-08 21:11:11 +0100

    Unit test with problem cases

--- UnitTests/Geometry/ConvexHullBuilderTest.cpp
+++ UnitTests/Geometry/ConvexHullBuilderTest.cpp
@@ -196,4 +196,60 @@ TEST_SUITE("ConvexHullBuilderTest")
 			CHECK(max_error < max(coplanar_distance, 1.2f * cTolerance));
 		}
 	}
+
+	TEST_CASE("TestHullEdgeCases")
+	{
+		const char *error = nullptr;
+
+		Positions positions[] = {
+			{
+				// A hull with 2 faces that are nearly coplanar
+				Vec3(-0.020472288f, -0.195635557f, 0.308015466f),
+				Vec3(0.136248738f, 0.633286834f, 0.135366619f),
+				Vec3(0.286418647f, -0.228475571f, 0.308084548f),
+				Vec3(-0.267285109f, 1.024676085f, 0.308042824f),
+				Vec3(0.396568149f, -0.971658647f, 0.308055162f),
+				Vec3(0.321081549f, -1.024676085f, 0.308036327f),
+				Vec3(0.034643859f, -0.404506862f, 0.308015764f),
+				Vec3(0.189224690f, -0.252762139f, 0.308060408f),
+			},
+			{
+				// Nearly coplanar points
+				Vec3(0.917345762f, 0.157111734f, 1.650970459f),
+				Vec3(-0.098074198f, 0.157116055f, 0.664742708f),
+				Vec3(1.777100325f, 0.157112047f, 1.238879442f),
+				Vec3(2.114324570f, 0.157112464f, 0.780688763f),
+				Vec3(1.926570415f, 0.157114446f, 0.240761161f),
+				Vec3(-1.045998096f, 0.157108605f, 1.548911095f),
+				Vec3(-1.820045233f, 0.157106474f, 1.050360918f),
+				Vec3(-1.918573976f, 0.157108605f, 0.039246202f),
+				Vec3(0.042619467f, 0.157113969f, -1.405336142f),
+				Vec3(0.575986624f, 0.157114401f, -1.370834589f),
+				Vec3(1.402592659f, 0.157115221f, -0.834864557f),
+				Vec3(1.110557318f, 0.157113969f, -1.336267948f),
+				Vec3(1.689781666f, 0.157115355f, -0.308773756f),
+				Vec3(2.205337524f, 0.157113209f, -0.281754494f),
+				Vec3(-1.346967936f, 0.157110974f, -0.978962541f),
+				Vec3(-1.346967936f, 0.157110974f, -0.978962541f),
+				Vec3(-2.085033417f, 0.157106936f, -0.506602883f),
+				Vec3(-0.981224537f, 0.157110706f, -1.445893764f),
+				Vec3(-0.481085658f, 0.157112658f, -1.426232934f),
+				Vec3(-0.981224537f, 0.157110706f, -1.445893764f),
+			}
+		};
+
+		for (const Positions &p : positions)
+		{
+			// Build hull
+			ConvexHullBuilder builder(p);
+			CHECK(builder.Initialize(INT_MAX, cTolerance, error) == ConvexHullBuilder::EResult::Success);
+
+			// Calculate error
+			Face *error_face;
+			float max_error, coplanar_distance;
+			int error_position_idx;
+			builder.DetermineMaxError(error_face, max_error, error_position_idx, coplanar_distance);
+			CHECK(max_error < max(coplanar_distance, 1.2f * cTolerance));
+		}
+	}
 }
