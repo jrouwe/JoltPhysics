a5c6d2d269332c9a63a0cd3cc6c6842e11b1b7a4
Jorrit Rouwe <jrouwe@gmail.com>
2026-05-16 20:25:54 +0200

    Giving up on 32 bit arm build on vs2026

--- Build/cmake_vs2026_cl_arm_32bit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cmake -S . -B VS2026_CL_ARM_32BIT -G "Visual Studio 18 2026" -A ARM -T v143 -DCMAKE_SYSTEM_VERSION="10.0.22621.0" -DCMAKE_CXX_COMPILER_WORKS=1 %*
-echo Note that Windows 11 SDK (10.0.22621.0) is the last SDK to support 32-bit ARM, make sure you have it installed.
-echo Open VS2026_CL_ARM_32BIT\JoltPhysics.slnx to build the project.
