38f93ec8fdc2e1f419de90f40d977218746da9b8
alkavan <igal@alkontek.com>
2026-05-18 10:11:35 +0300

    Adding options to Linux CMake build script, keeping the default behavior.
    * Adding `--shared` to build `.so` instead
    * Adding `--postfix-debug` to append `-d` to artifacts
    * Adding `--env-script` to source a script before running build

--- Build/cmake_linux_clang_gcc.sh
+++ Build/cmake_linux_clang_gcc.sh
@@ -1,28 +1,79 @@
 #!/bin/sh
 
-if [ -z $1 ]
-then
-	BUILD_TYPE=Debug
-else
-	BUILD_TYPE=$1
-	shift
+BUILD_TYPE=""
+COMPILER=""
+SHARED_LIB=false
+POSTFIX_DEBUG=false
+ENV_SCRIPT=""
+CMAKE_EXTRA_ARGS=""
+
+for arg in "$@"; do
+  case "$arg" in
+    --shared)
+      SHARED_LIB=true
+      ;;
+    --postfix-debug)
+      POSTFIX_DEBUG=true
+      ;;
+    --env-script=*)
+      ENV_SCRIPT="${arg#*=}"
+      ;;
+    *)
+      if [ -z "$BUILD_TYPE" ]; then
+        BUILD_TYPE="$arg"
+      elif [ -z "$COMPILER" ]; then
+        COMPILER="$arg"
+      else
+        CMAKE_EXTRA_ARGS="$CMAKE_EXTRA_ARGS $arg"
+      fi
+      ;;
+  esac
+done
+
+if [ -z "$BUILD_TYPE" ]; then
+  echo "No build type argument, using default BUILD_TYPE=Debug"
+  BUILD_TYPE="Debug"
+fi
+if [ -z "$COMPILER" ]; then
+  echo "No compiler argument, using default COMPILER=clang++"
+  COMPILER="clang++"
 fi
 
-if [ -z $1 ]
-then
-	COMPILER=clang++
-else
-	COMPILER=$1
-	shift
+echo ""
+echo "Usage: ./cmake_linux_clang_gcc.sh [BUILD_TYPE] [COMPILER] [--shared] [--postfix-debug] [--env-script=...]"
+echo "Build type options: Debug, Release, Distribution, ReleaseUBSAN, ReleaseASAN, ReleaseTSAN, ReleaseCoverage"
+echo "Compiler options: clang++, clang++-XX, g++, g++-XX where XX is the version"
+echo ""
+
+if [ -n "$ENV_SCRIPT" ]; then
+  . "$ENV_SCRIPT"
 fi
 
 BUILD_DIR=Linux_$BUILD_TYPE
+POSTFIX_ARG=""
+SHARED_ARG=""
+if [ "$BUILD_TYPE" = "Debug" ] && [ "$POSTFIX_DEBUG" = true ]; then
+  POSTFIX_ARG="-DCMAKE_DEBUG_POSTFIX=-d"
+fi
+if [ "$SHARED_LIB" = true ]; then
+  BUILD_DIR=${BUILD_DIR}_shared
+  SHARED_ARG="-DBUILD_SHARED_LIBS=ON"
+fi
+if [ -n "$POSTFIX_ARG" ]; then
+  CMAKE_EXTRA_ARGS="$CMAKE_EXTRA_ARGS $POSTFIX_ARG"
+fi
+if [ -n "$SHARED_ARG" ]; then
+  CMAKE_EXTRA_ARGS="$CMAKE_EXTRA_ARGS $SHARED_ARG"
+fi
 
-echo Usage: ./cmake_linux_clang_gcc.sh [Configuration] [Compiler]
-echo "Possible configurations: Debug (default), Release, Distribution, ReleaseUBSAN, ReleaseASAN, ReleaseTSAN, ReleaseCoverage"
-echo "Possible compilers: clang++, clang++-XX, g++, g++-XX where XX is the version"
 echo Generating Makefile for build type \"$BUILD_TYPE\" and compiler \"$COMPILER\" in folder \"$BUILD_DIR\"
+cmake -S . -B $BUILD_DIR -G "Unix Makefiles" \
+  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+  -DCMAKE_CXX_COMPILER=$COMPILER \
+  $CMAKE_EXTRA_ARGS
 
-cmake -S . -B $BUILD_DIR -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=$COMPILER "${@}"
-
-echo Compile by running \"make -j $(nproc) \&\& ./UnitTests\" in folder \"$BUILD_DIR\"
+echo ""
+echo Compile by running:
+echo "cd ${BUILD_DIR} && make -j$(nproc)"
+echo Run tests: ./UnitTests
+echo ""
