24f4c15275d0f54919fd72001010c8cbc4f43c88
Jorrit Rouwe <jrouwe@gmail.com>
2026-05-26 21:27:47 +0200

    Fixed ASAN build running under VC2026

--- Jolt/Core/LSANSuppressions.h
+++ Jolt/Core/LSANSuppressions.h
@@ -6,10 +6,16 @@
 
 #if defined(JPH_USE_VK) && defined(JPH_ASAN_ENABLED)
 
+JPH_SUPPRESS_WARNING_PUSH
+JPH_CLANG_SUPPRESS_WARNING("-Wreserved-identifier")
+
 // Suppress ASAN leak detection for the Vulkan driver
+extern "C" const char *__lsan_default_suppressions();
 extern "C" const char *__lsan_default_suppressions()
 {
 	return "leak:libvulkan";
 }
+
+JPH_SUPPRESS_WARNING_POP
 
 #endif
