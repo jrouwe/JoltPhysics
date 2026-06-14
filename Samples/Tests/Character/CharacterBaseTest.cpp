d1ec80504ecf289c6b6ac08a1c5bceb5533199c4
Jorrit Rouwe <jrouwe@gmail.com>
2026-06-14 14:14:26 +0200

    CharacterTest: Display contact information in red when horizontal speed limit is exceeded
    [skip ci]

--- Samples/Tests/Character/CharacterBaseTest.cpp
+++ Samples/Tests/Character/CharacterBaseTest.cpp
@@ -844,7 +844,8 @@ void CharacterBaseTest::DrawCharacterState(const CharacterBase *inCharacter, RMa
 	const PhysicsMaterial *ground_material = inCharacter->GetGroundMaterial();
 	Vec3 horizontal_velocity = inCharacterVelocity;
 	horizontal_velocity.SetY(0);
-	mDebugRenderer->DrawText3D(inCharacterTransform.GetTranslation(), StringFormat("State: %s\nMat: %s\nHorizontal Vel: %.1f m/s\nVertical Vel: %.1f m/s", CharacterBase::sToString(ground_state), ground_material->GetDebugName(), (double)horizontal_velocity.Length(), (double)inCharacterVelocity.GetY()), Color::sWhite, 0.25f);
+	Color color = horizontal_velocity.Length() > 1.01f * sCharacterSpeed? Color::sRed : Color::sWhite;
+	mDebugRenderer->DrawText3D(inCharacterTransform.GetTranslation(), StringFormat("State: %s\nMat: %s\nHorizontal Vel: %.1f m/s\nVertical Vel: %.1f m/s", CharacterBase::sToString(ground_state), ground_material->GetDebugName(), (double)horizontal_velocity.Length(), (double)inCharacterVelocity.GetY()), color, 0.25f);
 }
 
 void CharacterBaseTest::DrawPaddedCharacter(const Shape *inShape, float inPadding, RMat44Arg inCenterOfMass)
